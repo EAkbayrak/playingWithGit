64007ffe13d6fd9ddf1fa750116cd492d6ab4ad1
EAkbayrak <emreakbayrak94@gmail.com>
2022-09-26 08:23:40 +0100

    I love Java and the weather

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,8 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+        System.out.println("Today is a beautiful day");
+        System.out.println("But how much of a beautiful day is it");
+    }
+}
